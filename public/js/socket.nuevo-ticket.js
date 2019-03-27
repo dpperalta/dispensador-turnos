b5932c2d01f75c8cbfbdea09b4db8c57b6076545
Diego Peralta <die_go_p@hotmail.com>
2019-03-26 23:39:38 -0500

    Primer commit a producción

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,30 @@
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+
+    console.log('Conectado al Servidor');
+
+});
+
+socket.on('disconnect', function() {
+
+    console.log('Desconectado del Servidor');
+
+});
+
+socket.on('estadoActual', function(resp) {
+
+    console.log(resp);
+    label.text(resp.actual);
+
+});
+
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+    });
+
+});
